d9dcfe1d0788e74495613dd7394cd1ec243f9d91
Kirill Dzhumanov <Djum83@mail.ru>
2023-04-15 12:31:40 +0300

    feat: done count cats (Sat, Apr 15, 2023 12:31:40 PM)

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const matrix = [
+  [0, 1, '^^'],
+  [0, '^^', 2],
+  ['^^', 1, 2]
+];
+
+function countCats(matrix) {
+  let number = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '^^') {
+        number = number + 1;
+      }
+    }
+  }
+  return number;
 }
+
+console.log(countCats(matrix));
 
 module.exports = {
   countCats
