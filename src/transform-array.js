f8c8c2261efd976b5ec4fc24a62fa1695e613826
Djumanchik <djum83@mail.ru>
2023-04-17 10:46:54 +0300

    feat: transform-array 50% (17 апр 2023 г. 10:46:53)

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,18 +14,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 
-const arr = [1, 2, 3, '--double-next', 4, 5];
-console.log (arr);
+// const arr = { 'foo': 'bar' };
+// console.log (arr);
+// console.log(typeof arr);
 
 function transform(arr) {
-let result = [];
-for (let i = 0; i <= arr.length; i++) {
-    
+  let result = [];
+  if (typeof arr !== 'object' || typeof arr === 'undefined') {  // проверяем массив на соответсвие
+  throw new Error ("'arr' parameter must be an instance of the Array!");
+  } else if (arr[arr.length - 1] === '--double-next' || arr[arr.length - 1] === '--discard-next') { // удаляем оконечные инстуркции
+    arr.splice(1, 1); 
+  } else if (arr[0] === '--double-prev' || arr[0] === '--discard-prev') { // удаляем оконечные инстуркции
+    arr.splice(0, 1);
+  } else {
+    for (let i = 0; i <= arr.length - 1; i++) {
+      if (arr[i] === '--double-prev') {
+        result.push(arr[i - 1]);
+        arr.splice(i, 1);
+      }
+      if (arr[i] === '--double-next') {
+        result.push(arr[i + 1]);
+        arr.splice(i, 1);
+      }
+      if (arr[i] === '--discard-prev') {
+        result.pop(arr[i]);
+        arr.splice(i, 1);
+      }
+      if (arr[i] === '--discard-next') {
+        arr.splice(i, 2);
+      }
+        result.push(arr[i]);
+        console.log(result);
+      }
+    return result; 
   }
-  return result;
 };
 
-console.log(transform(arr));
+// console.log(transform(arr));
 
 module.exports = {
   transform
