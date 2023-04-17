a8b57f771e6ff3248c9d33ddaccd8fe6c2856468
Djumanchik <djum83@mail.ru>
2023-04-17 18:09:41 +0300

    feat: one bug in transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,41 +14,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 
-// const arr = [ 1, 2, 3, 1337, 4, 5 ];
-// console.log (arr);
+const arr = [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5];
+console.log (arr);
 // console.log(typeof arr);
 
 function transform(arr) {
   if (Array.isArray(arr) === false) {
     throw new Error ("'arr' parameter must be an instance of the Array!");
   }
+  let array = arr;
   let result = [];
-  if (arr[arr.length - 1] === '--double-next' || arr[arr.length - 1] === '--discard-next') { // удаляем оконечные инстуркции
-    arr.splice(-1, 1); 
-    console.log(arr);
-  } else if (arr[0] === '--double-prev' || arr[0] === '--discard-prev') { // удаляем оконечные инстуркции
-    arr.splice(0, 1);
+  if (array[array.length - 1] === '--double-next' || array[array.length - 1] === '--discard-next') { // удаляем оконечные инстуркции
+    array.splice(-1, 1); 
+    console.log(array);
+  } else if (array[0] === '--double-prev' || array[0] === '--discard-prev') { // удаляем оконечные инстуркции
+    array.splice(0, 1);
   }
-  for (let i = 0; i <= arr.length - 1; i++) {
-    if (typeof arr[i] === 'number') {
-      result.push(arr[i]);
-    } else if (arr[i] === '--double-prev') {
-      result.push(arr[i - 1]);
-    } else if (arr[i] === '--double-next') {
-      result.push(arr[i + 1]);
-    } else if (arr[i] === '--discard-prev') {
-      result.pop(arr[i]);
-    } else if (arr[i] === '--discard-next') {
-      arr.splice(i, 1);
-    } else if (typeof arr[i] === 'string' || 'boolean' || 'object' || 'symbol') {
-      result.push(arr[i]);
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (typeof array[i] === 'number') {
+      result.push(array[i]);
+    } else if (array[i] === '--double-prev') {
+      result.push(array[i - 1]);
+    } else if (array[i] === '--double-next') {
+      result.push(array[i + 1]);
+    } else if (array[i] === '--discard-prev') {
+      result.pop(array[i]);
+    } else if (array[i] === '--discard-next') {
+      array.splice(i, 2);
+    } else if (typeof array[i] === 'string' || 'boolean' || 'object' || 'symbol') {
+      // result.push(array[i]);
+      return arr;
     }  
     console.log(result);
   }
   return result; 
 };
 
-// console.log(transform(arr));
+console.log(transform(arr));
 
 module.exports = {
   transform
